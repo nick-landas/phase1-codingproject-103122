da67a2bc5e9840d5543331af0ebdab52d0224288
Nick Landas <nick.landas@gmail.com>
2022-11-16 00:05:29 -0600

    click is delivering desired data.

--- src/index.js
+++ src/index.js
@@ -2,73 +2,76 @@ document.body.className = 'fade';
 document.addEventListener('DOMContentLoaded',() => {
     window.setTimeout(function() {
         document.body.className = '';
-      }, 230);
+    }, 750);
     
-
-
-
-
     const resultsDiv = document.querySelector('#results');
-const apiSearchForm = document.querySelector('#api-search');
-
-
-apiSearchForm.addEventListener('submit', (e) => {
+    const apiSearchForm = document.querySelector('#api-search');
+    
+    apiSearchForm.addEventListener('submit', (e) => {
         e.preventDefault()
         const query = encodeURI(e.target.search.value)
-        const exampleQuery = document.querySelector('.artistName')
-        exampleQuery.innerHTML = `Search results for: ${query}`
+        const inputQuery = document.querySelector('.artistName')
+        inputQuery.innerHTML = `Search results for: ${query}`
         e.target.reset()
         resultsDiv.innerHTML =''
-        console.log(query)
+        //console.log(query)
         fetch(`https://api.artic.edu/api/v1/artworks/search?q=${query}&query[term][is_public_domain]=true&limit=10`)
         .then(res => res.json())    
         .then(artWorks => {
-            console.log(artWorks)
+           // console.log(artWorks)
             Object.keys(artWorks).forEach(key =>{
-                console.log(key); 
-                console.log(artWorks[key]);
+               //console.log(key); 
+                //console.log(artWorks[key]);
             })
-        artWorks.data.forEach(art => {
-            const div = document.createElement("div");
-            div.className = "container";
-            fetch(`${art.api_link}?fields=id,title,image_id,artist_title,style_title,classification_title`)
-            .then(r => r.json())
-            .then(metadata => {
-                console.log(metadata)
-                const artMeta = metadata;
-                const img = document.createElement("img");
-                const overlay = document.createElement('div')
-                overlay.className = "overlay";
-                img.setAttribute('class', "pictures")
-                const titleHover = document.createElement('div')
-                titleHover.className = "text";
-                titleHover.textContent = metadata.data.title
-                img.src = `https://www.artic.edu/iiif/2/${artMeta.data.image_id}/full/843,/0/default.jpg`
-                img.alt= art.thumbnail.alt_text
-                overlay.addEventListener('click', () => {
-                   
-                    console.log('Hello')
-                    
-                })
-                const h2 = document.createElement("h2");
-                h2.textContent = art.title
-            
-                const h3 = document.createElement("h3");
-                h3.textContent = art.id
-            
-            
-                const p = document.createElement("p");
-                // p.textContent = art.thumbnail.alt_text
-                overlay.append(titleHover);
-                div.append(img,overlay);
-                resultsDiv.append(div);
-            })
-        })
-        
-    
-        })
+            artWorks.data.forEach(art => {
+                    const div = document.createElement("div");
+                    div.className = "container";
+                    fetch(`${art.api_link}?fields=id,title,image_id,artist_title,style_title,classification_title`)
+                    .then(r => r.json())
+                    .then(metadata => {
+                        console.log(metadata)
+                        const artMeta = metadata;
+                        const img = document.createElement("img");
+                        const overlay = document.createElement('div')
+                        overlay.className = "overlay";
+                        img.setAttribute('class', "pictures")
+                        const titleHover = document.createElement('div')
+                        titleHover.className = "text";
+                        titleHover.textContent = metadata.data.title
+                        img.src = `https://www.artic.edu/iiif/2/${artMeta.data.image_id}/full/843,/0/default.jpg`
+                        img.alt= art.thumbnail.alt_text
 
-        
-})
+                        overlay.addEventListener('click', (e) => {
+                            
+                            console.log('Hello')
+                            const hiddenInfo = document.getElementById('facts')
+                            hiddenInfo.innerHTML =''
+                            const metaBox = document.createElement('ul')
+
+                            const artist = document.createElement('li')
+                            artist.innerText = artMeta.data.artist_title
+
+                            const style = document.createElement('li')
+                            style.innerText = artMeta.data.style_title
+
+                            const classification = document.createElement('li')
+                            classification.innerText = artMeta.data.classification_title
+
+                            console.log(artist.innerText)
+
+                            metaBox.append(artist,style,classification)
+                            hiddenInfo.append(metaBox)
+                            setTimeout(() => {
+                                //console.log('Hello World!');
+                                hiddenInfo.innerHTML =''
+                              }, 1500);
+                        })
+                            overlay.append(titleHover);
+                            div.append(img,overlay);
+                               resultsDiv.append(div);
+                    })
+            })
+        })   
+    })
 })
 
